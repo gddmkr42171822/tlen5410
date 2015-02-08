8f1151859726cf55c3b1d23d0da338f801288dae
TlEN 5410 Network Management <netman@netman.(none)>
2015-02-07 22:36:23 -0700

    added docstring to lab2

--- lab2/lab2_packet.py
+++ lab2/lab2_packet.py
@@ -3,10 +3,20 @@ This file defines the Packet class and its methods
 '''
 class Packet(object):
     def __init__(self, src, dst, payload):
+        '''
+        Attributes:
+            src -> source address
+            dst -> destination address
+            payload -> packet data
+        '''
         self.src = src
         self.dst = dst
         self.payload = payload
 
     def __str__(self):
+        '''
+        Prints the source address, destination address, and payload
+        of a packet
+        '''
         return '\nsrc: {0} dst: {1} payload: {2}'.format(self.src, self.dst,\
         self.payload)
--- lab2/lab2_switch.py
+++ lab2/lab2_switch.py
@@ -10,19 +10,32 @@ class Switch(object):
         self.forwardtable = {}
 
     def __str__(self):
+        '''
+        Prints the hostname and address of the switch
+        '''
         return 'hostname: {0} address: {1}'.format(self.hostname, \
         self.address)
 
     def printPorts(self):
+        '''
+        Prints the ports and associated hosts of the switch
+        '''
         print '\n' + self.hostname + ' port list:'
         for port, host in enumerate(self.ports):
             print 'port: {0} hostname: {1}'.format(port, host.hostname)
 
     def printForwardtable(self):
+        '''
+        Prints the mac address table (address and associated port)
+        of the switch
+        '''
         print '\n' + self.hostname + ' forwardtable:'
         for address, port in self.forwardtable.iteritems():
             print 'address: {0} port: {1}'.format(address, port)
 
     def connect(self, device):
+        '''
+        Connects a switch to a device
+        '''
         self.ports.append(device)
 
