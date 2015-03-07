cbd5f7bd84d25cfbb6b0a29c4f2c0465e3443c3b
TlEN 5410 Network Management <netman@netman.(none)>
2015-03-07 01:13:40 -0700

    updated lab3

--- lab3/SNMPTool.py
+++ lab3/SNMPTool.py
@@ -5,23 +5,85 @@ class Router(object):
     def __init__(self, desthost, community):
         self.session = netsnmp.Session(DestHost=desthost, Community=community,
                                 Version = 1)
+        self.sysname = netsnmp.Varbind('.1.3.6.1.2.1.1.5.0')
+        self.syscontact = netsnmp.Varbind('.1.3.6.1.2.1.1.4.0')
+        self.syslocation = netsnmp.Varbind('.1.3.6.1.2.1.1.6.0')
 
     def retrieveHostname(self):
-        sysname = netsnmp.Varbind('.1.3.6.1.2.1.1.5.0')
-        varlist = netsnmp.VarList(sysname)
+        varlist = netsnmp.VarList(self.sysname)
         self.session.get(varlist)
-        return sysname.val
+        return self.sysname.val
+
+    def setHostname(self, hostname):
+        self.retrieveHostname()
+        self.sysname.val = hostname
+        varlist = netsnmp.VarList(self.sysname)
+        self.session.set(varlist)
 
     def retrieveContact(self):
-        syscontact = netsnmp.Varbind('.1.3.6.1.2.1.1.4.0')
-        varlist = netsnmp.VarList(syscontact)
+        varlist = netsnmp.VarList(self.syscontact)
         self.session.get(varlist)
-        return syscontact.val
+        return self.syscontact.val
+
+    def setContact(self, contact):
+        self.retrieveContact()
+        self.syscontact.val = contact
+        varlist = netsnmp.VarList(self.syscontact)
+        self.session.set(varlist)
+
+
+    def retrieveUptime(self):
+        sysuptime = netsnmp.Varbind('.1.3.6.1.2.1.1.3.0')
+        varlist = netsnmp.VarList(sysuptime)
+        self.session.get(varlist)
+        return sysuptime.val
+
+
+    def retrieveLocation(self):
+        varlist = netsnmp.VarList(self.syslocation)
+        self.session.get(varlist)
+        return self.syslocation.val
+
+    def setLocation(self, location):
+        self.retrieveLocation()
+        self.syslocation.val = location
+        varlist = netsnmp.VarList(self.syslocation)
+        self.session.set(varlist)
+
+    def retrieveRouteTable(self):
+        ifnumber = netsnmp.Varbind('.1.3.6.1.2.1.2.1.0')
+        vl = netsnmp.VarList(ifnumber)
+        self.session.get(vl)
+        iproutedest = netsnmp.Varbind('.1.3.6.1.2.1.4.24.4.1.1')
+        iproutemask = netsnmp.Varbind('.1.3.6.1.2.1.4.24.4.1.2')
+        iproutenexthop = netsnmp.Varbind('.1.3.6.1.2.1.4.24.4.1.4')
+        varlist = netsnmp.VarList()
+        varlist.append(iproutedest)
+        varlist.append(iproutemask)
+        varlist.append(iproutenexthop)
+        destlist = []
+        masklist = []
+        hoplist = []
+        finallist = []
+        for i in range(0, int(ifnumber.val)-1):
+            self.session.getnext(varlist)
+            destlist.append(iproutedest.val)
+            masklist.append(iproutemask.val)
+            hoplist.append(iproutenexthop.val)
+        finallist.append(destlist)
+        finallist.append(masklist)
+        finallist.append(hoplist)
+        print len(finallist[0])
+        return finallist
+
 
 def main():
     r = Router('198.51.100.3', 'password')
     print r.retrieveHostname()
     print r.retrieveContact()
+    print r.retrieveLocation()
+    print r.retrieveRouteTable()
+
 
 if __name__ == '__main__':
     main()
--- lab3/web.py
+++ lab3/web.py
@@ -14,8 +14,42 @@ def monitor():
         router = SNMPTool.Router(ip, 'password')
         sysname = router.retrieveHostname()
         syscontact = router.retrieveContact()
-        return flask.render_template('monitor.html', router=ip, sysname=sysname,
-                                syscontact=syscontact)
+        sysuptime = router.retrieveUptime()
+        syslocation = router.retrieveLocation()
+        routetable = router.retrieveRouteTable()
+        lsize=len(routetable[0])
+        return flask.render_template('monitor.html', router=ip,
+                    sysname=sysname, syscontact=syscontact,
+                    sysuptime = sysuptime, syslocation = syslocation,
+                    routetable=routetable, destlist=routetable[0],
+                    masklist=routetable[1], hoplist=routetable[2],
+                    lsize=lsize)
+
+@app.route("/change", methods=['GET', 'POST'])
+def change():
+    if flask.request.method == 'POST':
+        ip = flask.request.form['routerip']
+        router = SNMPTool.Router(ip, 'password')
+        if flask.request.form['attribute'] == 'hostname':
+            sysname = router.setHostname(flask.request.form['newvalue'])
+        elif flask.request.form['attribute'] == 'contact':
+            syscontact = router.setContact(flask.request.form['newvalue'])
+        elif flask.request.form['attribute'] == 'location':
+            syslocation = router.setLocation(flask.request.form['newvalue'])
+        sysname = router.retrieveHostname()
+        syscontact = router.retrieveContact()
+        sysuptime = router.retrieveUptime()
+        syslocation = router.retrieveLocation()
+        routetable = router.retrieveRouteTable()
+        lsize=len(routetable[0])
+        return flask.render_template('monitor.html', router=ip,
+                    sysname=sysname, syscontact=syscontact,
+                    sysuptime = sysuptime, syslocation = syslocation,
+                    routetable=routetable, destlist=routetable[0],
+                    masklist=routetable[1], hoplist=routetable[2],
+                    lsize=lsize)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
