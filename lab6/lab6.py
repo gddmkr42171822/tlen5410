66ead5cd8321969d6adfd31f639c67d44828021f
TlEN 5410 Network Management <netman@netman.(none)>
2015-04-24 13:19:33 -0600

    added a remove user function

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,4 +1,7 @@
 '''
+Name:
+
+Lab 6
 
 Sources:
 1. https://stackoverflow.com/questions/19419754/how-to-remove-a-node-inside-an-iterator-in-python-xml-etree-elementree
@@ -38,63 +41,65 @@ password = 'netman'
 # Get more information
 #paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
 
-# Establish the connection
 def establish_connection(hostname, username, password):
+    '''
+    Establish a connection to a router
+    '''
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname, 22, username, password, allow_agent=False, look_for_keys=False)
     return client
 
-client = establish_connection(hostname, username, password)
-transport = client.get_transport()
-channel = transport.open_channel('session')
-channel.invoke_subsystem('netconf')
-
-data = ""
-while True:
-    if data.find(']]>]]>') != -1:
-        data = data.replace(']]>]]>', '')
-        break
-
-    data = channel.recv(1024)
-#print data.strip()
-
-channel.send(hello)
-channel.send(get_config_request)
-
-data = ""
-while True:
-    if data.find(']]>]]>') != -1:
-        data = data.replace(']]>]]>', '')
-        break
-
-    data += channel.recv(1024)
-
-#print data.strip()
-
-try:
-    tree = etree.fromstring(data)
-    print tree.findall('.//user')
-    for login in tree.iter('login'):
-        for user in login.iter('user'):
-            if user.find('name').text == 'admin':
-                login.remove(user)
-    print tree.findall('.//user')
+def remove_user(data, user_name):
     '''
-    for user in tree.findall('.//user'):
-        if user.find('name').text == 'admin':
-            tree.findall('.//user').remove(user)
-            print tree.findall('.//user')
+    Remove a user from the xml element tree
     '''
-    #tree = etree.tostring(tree)
-    #print tree
+    try:
+        tree = etree.fromstring(data)
+        for login in tree.iter('login'):
+            for user in login.iter('user'):
+                if user.find('name').text == user_name:
+                    login.remove(user)
+    except xml.parsers.expat.ExpatError, ex:
+        print ex
+    return tree
 
-except xml.parsers.expat.ExpatError, ex:
-    print ex
+def main():
+    client = establish_connection(hostname, username, password)
+    transport = client.get_transport()
+    channel = transport.open_channel('session')
+    channel.invoke_subsystem('netconf')
 
-# Create the shell channel, execute command & wait for response
-#(stdin, stdout, stderr) = client.exec_command('show ip int br')
-#for line in stdout.readlines():
-#    print line
-client.close()
+    data = ""
+    while True:
+        if data.find(']]>]]>') != -1:
+            data = data.replace(']]>]]>', '')
+            break
+        data = channel.recv(1024)
+    #print data.strip()
+
+    channel.send(hello)
+    channel.send(get_config_request)
+
+    data = ""
+    while True:
+        if data.find(']]>]]>') != -1:
+            data = data.replace(']]>]]>', '')
+            break
+        data += channel.recv(1024)
+    #print data.strip()
+
+    tree = remove_user(data, 'admin')
+    tree = etree.tostring(tree)
+    print tree
+
+    # Create the shell channel, execute command & wait for response
+    #(stdin, stdout, stderr) = client.exec_command('show ip int br')
+    #for line in stdout.readlines():
+    #    print line
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
