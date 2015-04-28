beee8ad7b933f0687a6a2d420aa785a235a39378
TlEN 5410 Network Management <netman@netman.(none)>
2015-04-27 20:59:23 -0600

    finished lab6

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -8,13 +8,22 @@ Sources:
 - How remove a child node with an iterator to the parent
 2. http://lxml.de/1.3/tutorial.html
 - How to insert an element in the tree
+3. https://groups.google.com/forum/#!topic/comp.lang.python/Rq40dmwLfMQ
+- How to remove the first and last line of a string
 '''
 
 import paramiko
 import xml.etree.ElementTree as etree
 import xml.parsers.expat
 
-hello = '''<?xml version="1.0" encoding="UTF-8"?>
+
+username = 'netman'
+password = 'netman'
+hosts = ['172.20.74.238', '172.20.74.239', '172.20.74.240',
+            '172.20.74.241', '172.20.74.242']
+
+hello = '''
+<?xml version="1.0" encoding="UTF-8"?>
 <hello>
     <capabilities>
         <capability>
@@ -22,7 +31,8 @@ hello = '''<?xml version="1.0" encoding="UTF-8"?>
     </capability>
 </capabilities>
 </hello>
-]]>]]>'''
+]]>]]>
+'''
 
 get_config_request = '''
 <?xml version="1.0" encoding="UTF-8"?>
@@ -36,133 +46,152 @@ get_config_request = '''
 ]]>]]>
 '''
 
-hostname = '172.20.74.239'
-username = 'netman'
-password = 'netman'
+oureditconfig_template = '''
+<rpc>
+    <edit-config>
+        <target>
+            <running/>
+        </target>
+        <config>
+'''
 
-# Get more information
-#paramiko.common.logging.basicConfig(level=paramiko.common.DEBUG)
+class Router(object):
+    def __init__(self):
+        self.compliant = True
+        self.violations = []
 
-def establish_connection(hostname, username, password):
-    '''
-    Establish a connection to a router
-    '''
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname, 22, username, password, allow_agent=False, look_for_keys=False)
-    return client
+    def establish_connection(self, hostname, username, password):
+        '''
+        Establish a connection to a router
+        '''
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, 22, username, password, allow_agent=False, look_for_keys=False)
+        self.client = client
 
-def remove_http(data):
-    '''
-    Remove http if is enabled
-    '''
-    try:
-        tree = etree.fromstring(data)
-        for web_management in tree.iter('web-management'):
-            for http in web_management.iter('http'):
-                print 'Disabling the HTTP Service'
-                web_management.remove(http)
-    except xml.parsers.expat.ExpatError, ex:
-        print ex
-    return tree
+    def establish_channel(self):
+        transport = self.client.get_transport()
+        self.channel = transport.open_channel('session')
+        self.channel.invoke_subsystem('netconf')
 
-def remove_user(data, user_name):
-    '''
-    Remove a user from the xml element tree
-    '''
-    try:
-        tree = etree.fromstring(data)
-        for login in tree.iter('login'):
-            for user in login.iter('user'):
-                if user.find('name').text == user_name:
-                    print 'Removing User Bob Kool'
-                    login.remove(user)
-    except xml.parsers.expat.ExpatError, ex:
-        print ex
-    return tree
+    def receive_data(self):
+        data = ""
+        while True:
+            if data.find(']]>]]>') != -1:
+                data = data.replace(']]>]]>', '')
+                break
+            data += self.channel.recv(1024)
+        self.data = data
 
-def change_mtu(data):
-    '''
-    Change the MTU to 1500
-    '''
-    try:
-        tree = etree.fromstring(data)
-        for interfaces in tree.iter('interfaces'):
-            for interface in interfaces.iter('interface'):
-                if 'ge' in interface.find('name').text:
-                    for unit in interface.iter('unit'):
-                        if unit.find('mtu') is not None:
-                            if unit.find('mtu').text != '1500':
-                                print 'Set the MTU for ' + interface.find('name').text + ' to 1500'
+    def remove_reply(self):
+        '''
+        Remove rpc-reply and configuration from the request
+        '''
+        self.data = self.data.replace('<rpc-reply>', '')
+        self.data = self.data.replace('</rpc-reply>', '')
+        self.data = self.data.strip('\n')
+
+    def remove_http(self):
+        '''
+        Remove http if is enabled
+        '''
+        try:
+            tree = etree.fromstring(self.data)
+            for web_management in tree.iter('web-management'):
+                for http in web_management.iter('http'):
+                    self.violations.append('Disabled the HTTP Service')
+                    self.compliant = False
+                    web_management.remove(http)
+        except xml.parsers.expat.ExpatError, ex:
+            print ex
+        self.data = etree.tostring(tree)
+
+    def remove_user(self):
+        '''
+        Remove a user from the xml element tree
+        '''
+        try:
+            tree = etree.fromstring(self.data)
+            for login in tree.iter('login'):
+                for user in login.iter('user'):
+                    if user.find('name').text == 'bkool':
+                        self.compliant = False
+                        self.violations.append('Removed User Bob Kool')
+                        login.remove(user)
+        except xml.parsers.expat.ExpatError, ex:
+            print ex
+        self.data = etree.tostring(tree)
+
+    def change_mtu(self):
+        '''
+        Set the MTU to 1500
+        '''
+        try:
+            tree = etree.fromstring(self.data)
+            for interfaces in tree.iter('interfaces'):
+                for interface in interfaces.iter('interface'):
+                    if 'ge' in interface.find('name').text:
+                        for unit in interface.iter('unit'):
+                            if unit.find('mtu') is not None:
+                                if unit.find('mtu').text != '1500':
+                                    self.violations.append('Set the MTU for ' + interface.find('name').text + ' to 1500')
+                                    self.compliant = False
+                                    unit.find('mtu').text = '1500'
+                            else:
+                                unit.insert(1, etree.Element('mtu'))
+                                self.violations.append('Set the MTU for ' + interface.find('name').text + ' to 1500')
+                                self.compliant = False
                                 unit.find('mtu').text = '1500'
-                        else:
-                            unit.insert(1, etree.Element('mtu'))
-                            print 'Set the MTU for ' + interface.find('name').text + ' to 1500'
-                            unit.find('mtu').text = '1500'
-    except xml.parsers.expat.ExpatError, ex:
-        print ex
-    return tree
+        except xml.parsers.expat.ExpatError, ex:
+            print ex
+        self.data = etree.tostring(tree)
 
-def change_snmp(data):
-    '''
-    Change the SNMP community string to read-only
-    '''
-    try:
-        tree = etree.fromstring(data)
-        for community in tree.iter('community'):
-            print community.find('name').text
-            print community.find('authorization').text
-            if community.find('authorization').text != 'read-only':
-                community.find('authorization').text = 'read-only'
-                print 'Set the SNMP community string' + community.find('name').text + ' to be read-only'
-    except xml.parsers.expat.ExpatError, ex:
-        print ex
-    return tree
+    def change_snmp(self):
+        '''
+        Change the SNMP community string to read-only
+        '''
+        try:
+            tree = etree.fromstring(self.data)
+            for community in tree.iter('community'):
+                if community.find('authorization').text != 'read-only':
+                    community.find('authorization').text = 'read-only'
+                    self.violations.append('Set the SNMP community string ' + community.find('name').text + ' to be read-only')
+                    self.compliant = False
+        except xml.parsers.expat.ExpatError, ex:
+            print ex
+        self.data = etree.tostring(tree)
+
+    def fix_xml(self):
+        self.remove_http()
+        self.remove_user()
+        self.change_mtu()
+        self.change_snmp()
+        self.remove_reply()
+
+    def edit_config(self):
+        self.data = oureditconfig_template + self.data + '\n</config>\n</edit-config>\n</rpc>\n]]>]]>\n'
+        self.channel.send(self.data)
+        self.receive_data()
 
 def main():
-    client = establish_connection(hostname, username, password)
-    transport = client.get_transport()
-    channel = transport.open_channel('session')
-    channel.invoke_subsystem('netconf')
-
-    data = ""
-    while True:
-        if data.find(']]>]]>') != -1:
-            data = data.replace(']]>]]>', '')
-            break
-        data = channel.recv(1024)
-    #print data.strip()
-
-    channel.send(hello)
-    channel.send(get_config_request)
-
-    data = ""
-    while True:
-        if data.find(']]>]]>') != -1:
-            data = data.replace(']]>]]>', '')
-            break
-        data += channel.recv(1024)
-    print data.strip()
-
-    '''
-    tree = change_mtu(data)
-    data = etree.tostring(tree)
-    tree = remove_user(data, 'bkool')
-    data = etree.tostring(tree)
-    tree = remove_http(data)
-    data = etree.tostring(tree)
-    '''
-    tree = change_snmp(data)
-    data = etree.tostring(tree)
-    print data
-
-    # Create the shell channel, execute command & wait for response
-    #(stdin, stdout, stderr) = client.exec_command('show run')
-    #for line in stdout.readlines():
-        #print line
-    client.close()
-
+    for host in hosts:
+        router = Router()
+        router.establish_connection(host, username, password)
+        router.establish_channel()
+        router.channel.send(hello)
+        router.receive_data()
+        router.channel.send(get_config_request)
+        router.receive_data()
+        router.fix_xml()
+        router.edit_config()
+        if router.compliant:
+            print '\nScanning the configuration for ' + host + '... OKAY'
+        else:
+            print '\nScanning the configuration for ' + host + '... NON-COMPLIANT'
+            for violation in router.violations:
+                print violation
+        router.client.close()
 
 if __name__ == '__main__':
     main()
